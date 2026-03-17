b736b0d78fa02697ab51e323beb5154b50cd481e
Psyrax <psyrax@opiumgarden.org>
2026-03-17 00:42:38 -0600

    chore: add Pelican configuration

---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+SITENAME = 'oglabs'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Argentina/Buenos_Aires'
+DEFAULT_LANG = 'es'
+
+THEME = 'themes/oglabs'
+
+# Content paths
+ARTICLE_PATHS = ['blog', 'projects', 'photos']
+ARTICLE_EXCLUDE_PATHS = ['photos/images']
+
+# URL structure
+ARTICLE_URL = '{category}/{slug}/'
+ARTICLE_SAVE_AS = '{category}/{slug}/index.html'
+
+CATEGORY_URL = '{slug}/'
+CATEGORY_SAVE_AS = '{slug}/index.html'
+
+# Static assets: fotos de galería + imágenes de posts de blog/proyectos
+STATIC_PATHS = ['photos/images', 'images']
+
+# Disable unused pages
+PAGE_PATHS = []
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+
+# Feed settings (disabled for v1)
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Pagination
+DEFAULT_PAGINATION = False
